8f14517a6aaba04747d84a7311cf16d8298e094c
Arnaud Weyts <arnaud.weyts@gmail.com>
2017-01-24 14:01:09 +0100

    added polling

--- index.js
+++ index.js
@@ -6,8 +6,10 @@ const fetch = require('node-fetch');
 
 app.use(express.static('static'));
 
+let lastQuery;
+
 // fetches logs using fetch
-function fetchLogs(socket, inputData) {
+function fetchLogs(socket, inputData, callback = null) {
   const url = 'http://api.irail.be/logs';
 
   fetch(url)
@@ -15,12 +17,49 @@ function fetchLogs(socket, inputData) {
       return response.json();
     })
     .then(queryData => {
-      // filter and pass queries
-      emitSingleQueries(socket, filterQueries(queryData, inputData));
+      // pass queries to the callback
+      // not the first time though, because we don't know what our lastquery is yet
+      if (callback) {
+        emitSingleQueries(socket, filterQueries(callback(queryData), inputData));
+        // data refreshed and events emitted, start a new timeout
+        refreshQueries(socket, inputData);
+      }
+      else {
+        // initliaze lastQuery
+        // at this point the first dataset has been fetched
+        lastQuery = queryData[queryData.length - 1];
+      }
     })
     .catch(ex => {
       socket.emit('error', ex);
     });
+}
+
+// runs fetchLogs after 1s
+function refreshQueries(socket, inputData) {
+  setTimeout(fetchLogs, 1000, socket, inputData, getNewData);
+}
+
+// checks for every query
+function getNewData(data) {
+  let newData = [];
+  let lastQueryReached = false;
+  data.forEach(query => {
+    // push new query to the array
+    if (lastQueryReached) {
+      newData.push(query);
+    }
+    else {
+      // check if query equals last query
+      if (JSON.stringify(query) === JSON.stringify(lastQuery)) {
+        lastQueryReached = true;
+        // return because this query doesn't count
+        return;
+      }
+    }
+  });
+  lastQuery = data[data.length - 1];
+  return newData;
 }
 
 // filters the queries
@@ -38,6 +77,7 @@ function filterQueries(queryData, inputData) {
   });
 }
 
+// emit event for every query
 function emitSingleQueries(socket, data) {
   data.forEach(query => socket.emit('query', {
     origin: query.query.departureStop,
@@ -49,8 +89,10 @@ io.on('connection', function(socket) {
   // client passes object with arrivalStop, etc.
   // eventually make this the actual station object
   socket.on('fetchLogs', (inputData) => {
-    // fetch logs
+    // fetch logs for the first time
     fetchLogs(socket, inputData);
+    // start polling
+    refreshQueries(socket, inputData);
   });
 });
 
