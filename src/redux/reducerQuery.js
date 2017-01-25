9a0de897b522e638c5c43ed809de10b304ca5da7
Haroen Viaene <hello@haroen.me>
2017-01-25 01:08:08 +0100

    comment

--- src/redux/reducerQuery.js
+++ src/redux/reducerQuery.js
@@ -8,6 +8,8 @@ export default function(state = INITIAL_STATE, action) {
   switch (action.type) {
     case RECIEVE_QUERY:
       // somehow limit it to X here
+      // also get out double (via Set -> array ?)
+      // maybe reverse here
       return {
         ...state,
         queries: [...state.queries, action.payload]
