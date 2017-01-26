fad2a39ecc75a8ba4bf9c0a35dc9e0c2571b949e
Haroen Viaene <hello@haroen.me>
2017-01-26 14:14:17 +0100

    an animation

--- src/components/Pod.js
+++ src/components/Pod.js
@@ -2,10 +2,23 @@ import React, {Component} from 'react';
 import styled, {keyframes} from 'styled-components';
 import bee from '../img/bee.svg';
 
+const moveIn = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+
+  to {
+    transform: rotate(360deg);
+  }
+`;
+
 const Img = styled.img`
   width: 3em;
   height: 3em;
+  animation: ${moveIn} 2s linear infinite;
 `;
+
+
 
 export default class Pod extends Component {
   render() {
