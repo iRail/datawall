1f0e0fdfa4132646fc060f6b57497948b93b2aac
Haroen Viaene <hello@haroen.me>
2017-01-26 21:06:07 +0100

    werkt

--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -8,7 +8,7 @@ export const queryReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case RECEIVE_QUERIES:
       const queries = [...state.queries, action.payload];
-      queries.sort((a, b) => b.querytime - a.querytime);
+      queries.sort((a, b) => new Date(b.querytime) - new Date(a.querytime));
       return {...state, queries: queries.slice(0,7)};
     default:
       return state;
