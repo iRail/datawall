95ba0b6c58a18799a381399eac91daa7791ed6c1
Haroen Viaene <hello@haroen.me>
2017-01-26 14:10:06 +0100

    pod things

--- src/components/Pod.js
+++ src/components/Pod.js
@@ -1,11 +1,16 @@
 import React, {Component} from 'react';
+import styled, {keyframes} from 'styled-components';
+import bee from '../img/bee.svg';
+
+const Img = styled.img`
+  width: 3em;
+  height: 3em;
+`;
 
 export default class Pod extends Component {
   render() {
     return(
-      <div>
-        A single iRail bee
-      </div>
+      <Img src={bee} alt="a lookup"/>
     );
   }
 }
