3d7d0666ee3209f31a483a962d981f65f8912779
Haroen Viaene <hello@haroen.me>
2017-01-26 20:02:22 +0100

    sort on querytime

--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -8,7 +8,7 @@ export const queryReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case RECEIVE_QUERIES:
       const queries = [...state.queries, action.payload];
-      queries.sort((a, b) => a - b);
+      queries.sort((a, b) => a.querytime - b.querytime);
       return {...state, queries: queries.slice(0,7)};
     default:
       return state;
