76b5a32c1d631dac46584cb3add3ab537be2a354
Elias Meire <me@eliasmei.re>
2017-01-26 19:41:49 +0100

    Move action to redux

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,7 @@
-import React, {Component} from 'react';
-import {connect} from 'react-redux';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import styled from 'styled-components';
-import io from 'socket.io-client';
-
-import {fetchQuery} from '../redux/actions';
-import {API_BASE_URI, STATION_URI} from '../constants';
+import { listenToQueries } from '../redux/actions';
 
 import Scene from './Scene.js';
 import Footer from './Footer.js';
@@ -18,28 +15,22 @@ const Main = styled.main`
 
 class App extends Component {
   componentWillMount() {
-    this.startFetching();
-  }
-
-  startFetching() {
-    const fetchQueryProp = this.props.fetchQuery;
-
-    const socket = io(API_BASE_URI);
-
-    // socket.emit('fetchLogs', { stop: STATION_URI});
-    socket.on(STATION_URI, function(data) {
-      fetchQueryProp(data);
-    });
+    this.props.listenToQueries();
   }
 
   render() {
+    const { queries } = this.props;
     return (
       <Main>
-        <Scene />
-        <Footer />
+        <Scene queries={queries} />
+        <Footer queries={queries} />
       </Main>
     );
   }
 }
 
-export default connect(null, {fetchQuery})(App);
+const mapStateToProps = (state, ownProps) => ({
+  queries: state.queries
+});
+
+export default connect(mapStateToProps, { listenToQueries })(App);
--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,8 +1,20 @@
-export const RECEIVE_QUERIES = 'RECEIVE_QUERIES';
+import io from 'socket.io-client';
+import { API_BASE_URI, STATION_URI } from '../constants';
 
-export function fetchQuery(query) {
-  return {
-    type: RECEIVE_QUERIES,
-    payload: query
-  };
-}
+export const RECEIVE_QUERIES = 'RECEIVE_QUERIES';
+export const LISTEN_FOR_QUERIES = 'LISTEN_FOR_QUERIES';
+
+export const receiveQueries = (queries) => ({
+  type: RECEIVE_QUERIES,
+  payload: queries
+});
+
+export const listenToQueries = () => (
+  dispatch => {
+    const socket = io(API_BASE_URI);
+    // socket.emit('fetchLogs', { stop: STATION_URI});
+    socket.on(STATION_URI, function(queries) {
+      dispatch(receiveQueries(queries));
+    });
+  }
+);
