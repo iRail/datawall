80e7af05256531daf9bd49328427d70c21df135d
Arnaud Weyts <arnaud.weyts@gmail.com>
2017-01-26 17:29:02 +0100

    Positioned main station component

--- src/components/Hub.js
+++ src/components/Hub.js
@@ -3,22 +3,30 @@ import styled from 'styled-components';
 import hub from '../img/hub.svg'
 import {STATION} from '../constants';
 
-const Img = styled.img`
-  height: 20vh;
-`;
-
 const Wrapper = styled.div`
 `;
 
+const MainStation = styled.div`
+  height: 40vh;
+  width: 40vw;
+  background-image: url(${hub});
+  background-size: contain;
+  background-position: center;
+  background-repeat: no-repeat;
+  text-align: center;
+`;
+
 const Text = styled.span`
+  line-height: 30vh;
 `;
 
 export default class Hub extends Component {
   render() {
     return (
       <Wrapper>
-        <Text>{STATION.name}</Text>
-        <Img src={hub} alt={STATION.name} />
+        <MainStation>
+          <Text>{STATION.name}</Text>
+        </MainStation>
       </Wrapper>
     );
   }
