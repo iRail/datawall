cb04a10c4036da3c61aa4f7318c087e89bcf12dd
Haroen Viaene <hello@haroen.me>
2017-01-26 15:11:35 +0100

    animations

--- src/components/Pod.js
+++ src/components/Pod.js
@@ -6,26 +6,40 @@ import {times} from '../constants';
 
 const moveIn = keyframes`
   from {
-    transform: rotate(0deg);
+    transform: translateX(${0}) translateY(${0});
   }
 
   to {
-    transform: rotate(360deg);
+    transform: translateX(0) translateY(0);
+  }
+`;
+
+const buzz = keyframes`
+  from {
+    transform: scale(1) translateX(${Math.random()}) translateY(${Math.random()});
+  }
+
+  to {
+    transform: scale(${1 + Math.random()}) translateX(${Math.random()}) translateY(${Math.random()});
   }
 `;
 
 const Img = styled.img`
   width: 3em;
   height: 3em;
-  animation: ${moveIn} ${times.podAnimation} linear infinite;
+  animation: ${buzz} ${times.podAnimation} linear 1;
 `;
 
-
+const Wrapper = styled.div`
+  animation: ${moveIn} ${times.podAnimation} linear 1;
+`;
 
 export default class Pod extends Component {
   render() {
     return(
-      <Img src={pod} alt="a lookup"/>
+      <Wrapper>
+        <Img src={pod} alt="a lookup"/>
+      </Wrapper>
     );
   }
 }
