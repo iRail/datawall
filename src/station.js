a5562d671d56a5ee00cf3126acab5b20dd35b6f4
Haroen Viaene <hello@haroen.me>
2017-01-28 16:51:38 +0100

    find the direction

--- src/station.js
+++ src/station.js
@@ -2,13 +2,37 @@ import {STATION} from './constants';
 
 export const DIRECTIONS = {
   northeast: 'NORTHEAST',
-  nortwest: 'NORTHWEST',
+  northwest: 'NORTHWEST',
   southeast: 'SOUTHEAST',
   southwest: 'SOUTHWEST'
 };
 
+
+/**
+ * Get the direciton of a station
+ * 
+ * Take in account that the directions needs to be in an X-shape
+ * 
+ * @see DIRECTIONS
+ * @returns the direction a certain station is in
+ * @param {object} [{latitude, longitude}=stop]
+ */
 export function getDirection({latitude, longitude} = stop) {
-  console.log(latitude, longitude);
+  if (longitude > STATION.longitude) {
+    // WEST
+    if (latitude > STATION.latitude) {
+      return DIRECTIONS.northwest;
+    } else {
+      return DIRECTIONS.southwest;
+    }
+  } else {
+    // EAST
+    if (latitude > STATION.latitude) {
+      return DIRECTIONS.northeast;
+    } else {
+      return DIRECTIONS.southeast;
+    }
+  }
 }
 
 export function isCenter(stop) {
