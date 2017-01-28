8b3e95e5e63bbb530d51b21390ea8e551b4168bf
Haroen Viaene <hello@haroen.me>
2017-01-28 10:45:58 +0100

    correct name

--- src/constants.js
+++ src/constants.js
@@ -18,7 +18,7 @@ export const times = {
 
 export const STATION = {
   URI: 'http://irail.be/stations/NMBS/008892007',
-  name: 'Gent Sint-Pieters',
+  name: 'Gent-Sint-Pieters',
   longitude: 3.710675,
   latitude: 51.035896
 };
