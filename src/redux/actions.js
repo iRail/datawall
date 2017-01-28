3d1832e114f2cfb14ce679f09c529690ac545c28
Haroen Viaene <hello@haroen.me>
2017-01-28 11:31:26 +0100

    add a DELETE_VISIBLE action

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -3,6 +3,7 @@ import {API_BASE_URI, STATION} from '../constants';
 
 export const RECEIVE_QUERIES = 'RECEIVE_QUERIES';
 export const LISTEN_FOR_QUERIES = 'LISTEN_FOR_QUERIES';
+export const DELETE_VISIBLE = 'DELETE_VISIBLE';
 
 export const receiveQueries = (queries) => ({
   type: RECEIVE_QUERIES,
@@ -18,3 +19,8 @@ export const listenToQueries = () => (
     });
   }
 );
+
+export const deleteVisible = (index) => ({
+  type: DELETE_VISIBLE,
+  payload: index
+});
