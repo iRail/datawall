4927dd672300d0369be8a211e701572d36c5ae15
Haroen Viaene <hello@haroen.me>
2017-01-28 11:31:38 +0100

    implement DELETE_VISIBLE badly

--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -1,22 +1,41 @@
-import {RECEIVE_QUERIES} from './actions';
+import store from './store';
+import {RECEIVE_QUERIES, DELETE_VISIBLE, deleteVisible} from './actions';
+import {times} from '../constants';
 
 const INITIAL_STATE = {
   queries: [],
   visible: {}
 };
 
+let VISIBLE_INDEX = 0;
+
 export const queryReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case RECEIVE_QUERIES:
       const queries = [...state.queries, action.payload];
       queries.sort((a, b) => new Date(b.querytime) - new Date(a.querytime));
+
+      VISIBLE_INDEX++;
+
+      setTimeout(() => {
+        store.dispatch(deleteVisible(VISIBLE_INDEX));
+      }, times.podAnimation + 10);
+
       return {
         ...state,
         queries: queries.slice(0,7),
         visible: {
-          idx: queries[0]
-         } // todo all visible queries
+          ...state.visible,
+          [VISIBLE_INDEX]: action.payload[0]
+        }
       };
+    case DELETE_VISIBLE:
+      const visible = state.visible;
+      delete visible.action.payload;
+      return {
+        ...state,
+        visible
+      }
     default:
       return state;
   }
