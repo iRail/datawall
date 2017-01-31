859596ebabc920958af4fe7afe6bb20510e1dd85
Haroen Viaene <hello@haroen.me>
2017-01-31 16:39:29 +0100

    simplify "footer" style

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,27 +5,19 @@ import {colors, sizes, zIndex} from '../constants';
 
 import List from './List.js';
 
-const Wrapper = styled.footer`
+const Wrapper = styled.div`
   background-color: ${colors.black};
   color: ${colors.white};
-  z-index: ${zIndex.footer};
-  display: flex;
-  flex-basis: ${sizes.footer.height};
+  z-index: ${zIndex.list};
+  flex-basis: ${sizes.list.height};
 `; // basis should be whatever's needed for this to fit or a fraction of the height (x screens)
-
-const Container = styled.div`
-  flex-grow: 1;
-  padding: 1em;
-`;
 
 export default class Footer extends Component {
   render() {
     const {queries} = this.props;
     return(
       <Wrapper>
-        <Container>
-          <List queries={queries} />
-        </Container>
+        <List queries={queries} />
       </Wrapper>
     );
   }
