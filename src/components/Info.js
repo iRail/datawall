10b62bb74a3b8cd5ea8d37b28660f139ec69e1cb
Haroen Viaene <hello@haroen.me>
2017-02-01 14:58:17 +0100

    meme the logos

--- src/components/Info.js
+++ src/components/Info.js
@@ -1,48 +1,41 @@
 import React from 'react';
 import styled from 'styled-components';
 
-import {colors} from '../constants';
+// import {colors} from '../constants';
 
 import logo from '../img/logo_white.svg';
+import imec from '../img/logo_imec.svg';
 
 const Wrapper = styled.div`
   display: flex;
-  align-items: center;
+  align-items: space-between;
   padding: 0.8em;
   position: absolute;
   top: 0;
   left: 0;
-`;
-
-const Header = styled.header`
-  font-weight: 600;
-  margin: .4em;
-  font-size: 2vh;
-  color: ${colors.white};
+  width: 100%;
+  box-sizing: border-box;
 `;
 
 const Logo = styled.img`
-  width: 5vh;
+  height: 100%;
   margin: 0.4em;
 `;
 
-const Link = styled.a`
-  color: inherit;
-  text-decoration: none;
-`;
+// const Link = styled.a`
+//   color: inherit;
+//   text-decoration: none;
+// `;
 
-const Text = styled.p`
-  margin: 0;
-`
+// const Text = styled.p`
+//   margin: 0;
+// `
 
 export default () => {
   return (
     <Wrapper>
-      <Logo src={logo} alt="logo" />
-      <Header>
-        <Text>See your own search request</Text>
-        <Text>use <Link href="https://irail.be/route" target="_blank" rel="noopener">www.irail.be</Link></Text>
-      </Header>
+      <Logo src={logo} alt="iRail" />
+      <Logo src={imec} alt="IMEC" />
     </Wrapper>
   );
 }
