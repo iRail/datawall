c4d67199605e9f3f7c288e496ef03137365a15a4
Haroen Viaene <hello@haroen.me>
2017-02-01 14:02:00 +0100

    fix typo

--- src/img/hub/index.js
+++ src/img/hub/index.js
@@ -1,8 +1,8 @@
-import back from 'back.svg';
-import doors_back_front from 'doors_back_front.svg';
-import jar from 'jar.svg';
-import doors_front_back from 'doors_front_back.svg';
-import doors_front_front from 'doors_front_front.svg';
+import back from './back.svg';
+import doors_back_front from './doors_back_front.svg';
+import jar from './jar.svg';
+import doors_front_back from './doors_front_back.svg';
+import doors_front_front from './doors_front_front.svg';
 
 export default {
   back,
