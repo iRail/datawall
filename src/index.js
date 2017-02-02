99d1441c6618d5d4b6c694f0eaec6c8f45de4a43
Haroen Viaene <hello@haroen.me>
2017-02-02 14:53:41 +0100

    switch out pt sans from google to from npm

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
 
+import 'typeface-pt-sans';
 import './index.css';
 
 import store from './redux/store';
 import App from './components/App';
+
 
 ReactDOM.render(
   <Provider store={store}>
