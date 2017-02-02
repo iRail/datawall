6aca3efce14adf14c292e19e5d25e80f643fda37
Arnaud Weyts <arnaud.weyts@gmail.com>
2017-02-02 11:33:56 +0100

    added chevrons

--- src/img/icons/index.js
+++ src/img/icons/index.js
@@ -2,10 +2,14 @@ import left from './left.svg';
 import right from './right.svg';
 import inbound from './inbound.svg';
 import outbound from './outbound.svg';
+import chevronRight from './chevron-right.svg';
+import chevronLeft from './chevron-left.svg';
 
 export default {
   left,
   right,
   inbound,
   outbound,
+  chevronRight,
+  chevronLeft
 }
