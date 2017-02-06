b11c9d39e7e6c91904cc65f2de0beca458369242
Elias Meire <me@eliasmei.re>
2017-02-06 13:50:55 +0100

    remove deleteVisible action

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -3,7 +3,6 @@ import {API_BASE_URI} from '../constants';
 
 export const RECEIVE_QUERIES = 'RECEIVE_QUERIES';
 export const LISTEN_FOR_QUERIES = 'LISTEN_FOR_QUERIES';
-export const DELETE_VISIBLE = 'DELETE_VISIBLE';
 
 export const receiveQueries = (queries) => ({
   type: RECEIVE_QUERIES,
@@ -19,8 +18,3 @@ export const listenToQueries = () => (
     });
   }
 );
-
-export const deleteVisible = (index) => ({
-  type: DELETE_VISIBLE,
-  payload: index
-});
